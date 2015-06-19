555747d701e0a5d90715bdba36f775c19dee164c
David Sheehy <ds@schwey.io>
2015-06-19 16:01:47 -0500

    Finished 2.1

---
+++ ch2/formula1.hs
@@ -0,0 +1,4 @@
+p = True
+q = False
+
+formula1 = (not p) && (p ==> q) <=> not (q && (not p))
---
+++ ch2/tamo.hs
@@ -0,0 +1,17 @@
+-- implication
+infix 1 ==>
+(==>) :: Bool -> Bool -> Bool
+x ==> y = (not x) || y
+
+-- True ==> x  = x
+-- False ==> x = True
+
+-- equivilence p => q and q => p
+infix 1 <=>
+(<=>) :: Bool -> Bool -> Bool
+x <=> y = x == y
+
+-- exclusive or
+infixr 2 <+>
+(<+>) :: Bool -> Bool -> Bool
+x <+> y = x /= y
